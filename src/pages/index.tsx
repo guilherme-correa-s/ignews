ed0d12981147bd7619183d89c76cdda9b2fc3ddf
gui c <guilhermescorrea@hotmail.com.br>
2022-03-27 11:50:40 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +9,27 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1>Hello world</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {};
+};
