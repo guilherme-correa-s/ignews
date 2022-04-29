793c562e30c371fc9245530d5286e3486982efb8
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-29 15:53:54 -0300

    feat: stripe feature

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,40 @@
+import { query as q } from "faunadb";
+import { faunadb } from "../../../services/faunadb";
+import { stripe } from "../../../services/sripe";
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction: boolean = false
+) {
+  const userRef = await faunadb.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    priceId: subscription.items.data[0].price.id,
+  };
+  
+  if (createAction) {
+    await faunadb.query(
+      q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+    );
+  } else {
+    await faunadb.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscription_by_id"), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
+}
