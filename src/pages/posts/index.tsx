16e318b04ac43b8747ccd2e103dd69d3863ff325
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-30 18:07:46 -0300

    feat:  posts and preview posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,69 @@
+import { GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import { RichText } from "prismic-dom";
+import { createClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+export default function Posts({ posts }: PostsProps) {
+  const session = useSession()
+  const user = session.data?.activeSubscription
+  return (
+    <>
+      <Head>
+        <title>Posts | ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${user ? post.slug : 'preview/'+post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = createClient();
+  const response = await prismic.getAllByType("publication");
+
+  const posts = response.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
